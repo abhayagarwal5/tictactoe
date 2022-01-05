205064e1b106a73e13ba064c19369a6c22561a5d
abhayagarwal5 <abhayag1998@gmail.com>
2022-01-05 20:42:20 +0530

    calculate winner and lift up state

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,26 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Square from './Square';
-const Board = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setisXNext] = useState(false);
-  // console.log(board);
-  // console.log(setBoard);
-  const handelSquareClick = position => {
-    if (board[position]) {
-      return;
-    }
-    setBoard(prev => {
-      return prev.map((square, pos) => {
-        if (pos === position) {
-          return isXNext ? 'X' : 'O';
-        }
-        return square;
-      });
-    });
-    setisXNext(prev => {
-      return !prev;
-    });
-  };
+
+const Board = ({ board, handelSquareClick }) => {
   const renderSquare = position => {
     return (
       <Square
