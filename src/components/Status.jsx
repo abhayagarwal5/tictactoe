8f1e01f9efa855555e26468653872d2477b95f1b
abhayagarwal5 <abhayag1998@gmail.com>
2022-01-26 14:31:49 +0530

    styled the entire app

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
 
 const Status = ({ winner, current }) => {
-  // const message = winner
-  //   ? `Winner is ${winner}`
-  //   : `Next player is ${current.isXNext ? 'X' : 'O'}`;
   const noMovesLeft = current.board.every(element => {
     return element !== null;
   });
   return (
-    <h2>
-      {winner && `Winner is ${winner}`}
-      {!winner &&
-        !noMovesLeft &&
-        `Next player is ${current.isXNext ? 'X' : 'O'}`}
-      {!winner && noMovesLeft && 'X and O tied'}
-    </h2>
+    <div>
+      {winner && (
+        <>
+          Winner is{' '}
+          <span className={winner == 'X' ? 'text-green' : 'text-orange'}>
+            {winner}
+          </span>
+        </>
+      )}
+      {!winner && !noMovesLeft && (
+        <>
+          Next player is{' '}
+          <span className={current.isXNext ? 'text-green' : 'text-orange'}>
+            {current.isXNext ? 'X' : 'O'}
+          </span>
+        </>
+      )}
+      {!winner && noMovesLeft && (
+        <>
+          <span className="text-green">X</span> and{' '}
+          <span className="text-orange">O</span> tied
+        </>
+      )}
+    </div>
   );
 };
 
