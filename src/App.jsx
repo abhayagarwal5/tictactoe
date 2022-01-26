e4dadb40252f93588cba163d06f0b6a834b18e4a
abhayagarwal5 <abhayag1998@gmail.com>
2022-01-26 13:07:16 +0530

    highlited winning square and added start new game button

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,22 @@ import History from './components/History';
 import Status from './components/Status';
 import { calculateWinner } from './helper';
 import './styles/root.scss';
-
+const NEW_GAME = [{ board: Array(9).fill(null), isXNext: true }];
 const App = () => {
-  const [history, setHistorty] = useState([
-    { board: Array(9).fill(null), isXNext: true },
-  ]); //Storing the array of object for the histroty at that given point
+  const [history, setHistory] = useState(NEW_GAME); //Storing the array of object for the histroty at that given point
   // const [isXNext, setisXNext] = useState(false);
   // console.log(board);
   // console.log(setBoard);
   const [currentMove, setCurrentMove] = useState(0);
   const current = history[currentMove];
   // console.log('history', history);
-  const winner = calculateWinner(current.board);
+  const { winner, winningSquare } = calculateWinner(current.board);
 
   const handelSquareClick = position => {
     if (current.board[position] || winner) {
       return;
     }
-    setHistorty(prev => {
+    setHistory(prev => {
       const last = prev[prev.length - 1];
 
       const newBoard = last.board.map((square, pos) => {
@@ -40,11 +38,22 @@ const App = () => {
   const moveTo = move => {
     setCurrentMove(move);
   };
+  const newgame = () => {
+    setHistory(NEW_GAME);
+    setCurrentMove(0);
+  };
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
       <Status winner={winner} current={current} />
-      <Board board={current.board} handelSquareClick={handelSquareClick} />
+      <Board
+        board={current.board}
+        handelSquareClick={handelSquareClick}
+        winningSquare={winningSquare}
+      />
+      <button type="button" onClick={newgame}>
+        Start new game
+      </button>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
